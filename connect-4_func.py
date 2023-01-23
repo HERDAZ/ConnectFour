51b49f63b83d0c6823fec35dc211613b2d1f03f0
HERDAZ <paladindu38@gmail.com>
2023-01-23 19:19:02 +0100

    modified:   connect-4_func.py 	modified:   connect-4_oop.py

--- connect-4_func.py
+++ connect-4_func.py
@@ -2,19 +2,19 @@ def printGrid(grid: list, gridSize: list, symbols: list) -> None:
     """Print the grid in the CLI"""
     for i in range(gridSize[1]): # prints row's numbers
         print(f' {i+1} ', end='')
-    print('') # put the cursor bring the cursor down one line
+    print('') # bring the cursor bring the cursor down one line
     for i in (abs(y) for y in range(-gridSize[0], 0)):
-    # make if so 'i' will iterate form 'gridSize[0]' to 0
+    #'i' will iterate form 'gridSize[0]' to 0
         print('|', end='') 
-        for y in grid: # print each row, and places the corresponding symbol
-            if len(y) >= i: print('', symbols[y[i-1]], end='|')
+        for y in grid:
+            if len(y) >= i: print('', symbols[y[i-1]], end='|') # if the point exist, print it
             else: print('  ', end='|')
         print('')
 
-def columnsIsWin(collumn, player, winLenght: int) -> bool:
+def columnsIsWin(collumn: list, player: int, winLenght: int) -> bool:
     """Checks if the collumn just played contains an uninterupted line of 4
     player's points"""
-    if len(collumn) >= 4:
+    if len(collumn) >= winLenght:
     # there can not be a line of 4 point if the collumn is less than 4 point in lenght
         chain = 0
         # variable to stock the uninterputed line
@@ -25,7 +25,7 @@ def columnsIsWin(collumn, player, winLenght: int) -> bool:
     return False
     # return a False if there is no uninterupted collumn of 4
 
-def rowIsWin(row, player, grid: list, winLenght: int, gridSize: list) -> bool:
+def rowIsWin(row: int, player: int, grid: list, winLenght: int, gridSize: list) -> bool:
     """Checks if the row just played contains an uninterupted line of 4
     player's points"""
     chain = 0
@@ -42,46 +42,44 @@ def rowIsWin(row, player, grid: list, winLenght: int, gridSize: list) -> bool:
 
 def diagonalIsWin(originalCoord: int, player: int, grid: list, winLenght: int, gridSize: list) -> bool:
 
+    # check right to left diagonal
     originalCoord = [originalCoord-1, len(grid[originalCoord-1])-1]
     # convert the number of the line played by the coordonates of the last piece
     c = originalCoord
     # create a copy of 'originalCoord', to be used in the next 10 lines of code
     chain = 0
-    # initiate the counter for the lenght of uninterupted line of the player's pieces
     l = min(gridSize[1]-c[0], c[1]+1)
-    # the distance the cursor will have to travel to get to the base of the grid
+    # distance the cursor will have to travel to get to the base of the grid
     c = [c[0]+l-1, c[1]-l+1]
-    # create the base coodinate for the right to left diagonal
+    # base coordonates for the base of the right to left diagonal
     l = min(c[0]+1, gridSize[0]-c[1])
     # the distance the cursor will have to travel to see the entire diagonal
-    # to the tip of the grid
+    # to the tip or the side of the grid
     for i in range(l):
         try:
             if grid[c[0]-i][c[1]+i] == player: chain += 1
-            # checks if the point at 'grid[c[1]-i][c[0]+i]' is one of the player's piece
+            # goes thru all the point in the digonal, and check if they belong to the player
             else: chain = 0
             if chain == winLenght: return True
-            # stop the fonction and return True if there is an uniterupted line
-            # of four of the player's piece
         except IndexError: chain = 0
         # if the point dosn't exist, catch the error, and make 'chain' = 0
 
+    # check left to right diagonal
     c = originalCoord
     # create a copy of 'originalCoord', to be used in the next 10 lines of code
     chain = 0
     # initiate the counter for the lenght of uninterupted line of the player's pieces
     l = min(c[0]+1,c[1]+1)
-    # the distance the cursor will have to travel to get to the base of the grid
+    # distance the cursor will have to travel to get to the base of the grid
     c = [c[0]-l+1, c[1]-l+1]
-    # create the base coordonates for the left to right diagonal
+    # base coordonates for the base of the right to left diagonal
     l = min(gridSize[0]-c[0],gridSize[1]-c[1])
     # the distance the cursor will have to travel to see the entire diagonal to 
-    # the top of the grid
+    # the top or the side of the grid
     for i in range(l):
         try:
             if grid[c[0]+i][c[1]+i] == player: chain += 1
-            # check if the point at 'grid[c[1]+i][c[0]+i]' is one of the player's
-            # piece
+            # goes thru all the point in the digonal, and check if they belong to the player
             else: chain = 0
             if chain == winLenght: return True
             # stop the fonction and return True if ther is an uniterupted line
@@ -91,24 +89,23 @@ def diagonalIsWin(originalCoord: int, player: int, grid: list, winLenght: int, g
     return False
 
 def getGridDimentions() -> list:
-    '''Get dimention for the grid'''
+    '''Get dimention for the grid from the players'''
     while True:
         try:
             width = int(input('Width of the board : '))
-            if width < 4:
-                print('Please enter a number higher or equal to 4'); continue
+            if width < 5:
+                print('Please enter a number higher or equal to 5'); continue
             break
         except ValueError:
-        # execute if the input isn't a number
-            print('Please enter a number higher or equal to 4'); continue
+            print('Please enter a number higher or equal to 5'); continue
     while True:
         try:
             lenght = int(input('Lenght of the board : '))
-            if lenght < 3:
-                print('Please enter a number higher or equal to 3'); continue
+            if lenght < 4:
+                print('Please enter a number higher or equal to 4'); continue
             break
         except ValueError:
-            print('Please enter a number higher or equal to 3'); continue
+            print('Please enter a number higher or equal to 4'); continue
     return [lenght,width]
 
 def getPlayerSymbols(index: int) -> str:
@@ -117,26 +114,34 @@ def getPlayerSymbols(index: int) -> str:
         symbol =  input(f'Player {index+1}, please input your symbol : ')
         if symbol in ['', ' ']:
             print("Can't use this symbol, please input a valid symbol"); continue
-        symbol = symbol
         break
     return symbol
 
-def endOfGame():
+def getCoord(grid, playing, gridSize) -> int:
+    while True:
+        try:
+            coord = int(input(f"Player {playing+1} : "))
+        except ValueError: print('Wrong input, try again'); continue
+        # input is something other than a number
+        if coord not in range(1,gridSize[1]+1): print('Wrong input, try again'); continue 
+        # input is not out of bound of the grid
+        fullCollumns = list(filter(lambda x: len(grid[x-1])==gridSize[0], [i for i in range(1,gridSize[1]+1)]))
+        # list of full collumn 
+        if coord in fullCollumns: print('Full collumn, please inpyt another collumn'); continue
+        # if the collumn selected is full, ask the player for another input
+        break
+    return coord, fullCollumns
+
+def endOfGame() -> None:
     while True:
         stop = input('Continue ? (Y/N) : ').lower()
-        # used lower() to user-proof the input
-        if stop == 'y': return True
-        # return a True if the players want to restart another game
-        if stop == 'n': return False
-        # stop the python program if they don't want to
+        if stop == 'y': return 
+        if stop == 'n': exit('See you later :)')
         else: print('Wrong input, please try again')
-        # else, tell the players that the input is invald, and ask for another one
 
 def main():
     while True: #SessionLoop
-
         gridSize = getGridDimentions()
-        # get the grid dimention of the grid
         if gridSize[0]*gridSize[1] < 7*6: winLenght = 3
         # if the chozen size of the grid is smaller than the regular one, make the required
         # winning lenght smaller
@@ -145,44 +150,34 @@ def main():
         # winning lenght smaller
         else : winLenght = 4
         grid = [[] for _ in range(gridSize[1])]
-        # create the 2D grid
+        # create the empty 2D grid
         symbols = [getPlayerSymbols(0), getPlayerSymbols(1)]
-        # list to store player's symbol 
+        # store player's symbol 
         playing  = 0
-        # initiate the variable holding the position of the playing player symbol in 'symbols'
+        # the position of the playing player symbol in 'symbols'
 
         while True: # GameLoop
             printGrid(grid, gridSize, symbols)
-            # print the actuated grid
-            while True: # PlayLoop
-                try:
-                    coord = int(input(f"Player {playing+1} : "))
-                    # get input from player
-                except ValueError: print('Wrong input, try again'); continue
-                # if 'coord' isn't a integer, ask the player for another input
-                if coord not in range(1,gridSize[1]+1): print('Wrong input, try again'); continue
-                # if 'coord' doesn't have the right value, ask the player for another input
-                fullCollumns = list(filter(lambda x: len(grid[x-1])==gridSize[0], [i for i in range(1,gridSize[1]+1)]))
-                # get the list of full collumn by comparing their lenght with the grid lenght 
-                if coord in fullCollumns: print('Full collumn, please inpyt another collumn'); continue
-                # if the collumn selected is full, ask the player for another input
-                break
-                # get out of the loop if 
+            
+            coord, fullCollumns = getCoord(grid, playing, gridSize)
 
             grid[coord - 1].append(playing)
             # adds the player's piece to the collumn inputed
-            if rowIsWin(len(grid[coord - 1])-1, playing, grid, winLenght, gridSize) or columnsIsWin(grid[coord - 1], playing, winLenght) or diagonalIsWin(coord, playing, grid, winLenght,gridSize):
-            # checks there is an uniterupted line of 4 point of the playing player
+            if (rowIsWin(len(grid[coord - 1])-1, playing, grid, winLenght, gridSize) or
+                columnsIsWin(grid[coord - 1], playing, winLenght) or
+                diagonalIsWin(coord, playing, grid, winLenght,gridSize)):
+
                 printGrid(grid, gridSize, symbols)
                 print(f'Player {playing+1} won !')
                 break
+
             if fullCollumns == [i for i in range(1, gridSize[1]+1)]:
                 printGrid(grid)
                 print('All collumns are full, this is a tie.')
                 break
+
             playing = (playing + 1) % 2
             # switch player (0 to 1 and 1 to 0)
-        if endOfGame(): continue
-        else: exit('See you next time :)')
+        endOfGame()
         
 main()
--- connect-4_oop.py
+++ connect-4_oop.py
@@ -4,34 +4,24 @@ class Board:
         while True:
             try:
                 width = int(input('Width of the board : '))
-                # get the wanted width of the grid from the players
-                if width < 4: print('Please enter a number higher or equal to 4'); continue
-                # ask for another input if 'width' < 4
+                if width < 5: print('Please enter a number higher or equal to 5'); continue
                 self.width = width
                 break
-            except ValueError: print('Please enter a number higher or equal to 4'); continue
+            except ValueError: print('Please enter a number higher or equal to 5'); continue
             # ask for another input if 'width' isn't a number
         while True: # LenghtChoosingLoop
             try:
                 lenght = int(input('Lenght of the board : '))
-                # ask for another input if 'width' < 3
-                if lenght < 3: print('Please enter a number higher or equal to 3'); continue
+                if lenght < 4: print('Please enter a number higher or equal to 4'); continue
                 self.lenght = lenght
                 break
-            except ValueError: print('Please enter a number higher or equal to 3'); continue
+            except ValueError: print('Please enter a number higher or equal to 4'); continue
             # ask for another input if 'width' isn't a number
-                # create the 2D grid, where point can be accesses with
         if width*lenght < 7*6: self.winLenght = 3
-        # if the chozen size of the grid is smaller than the regular one, make the required
-        # winning lenght smaller
         elif width*lenght > 7*6: self.winLenght = 5
-        # if the chozen size of the grid is bigger than the regular one, make the required
-        # winning lenght bigger
         else: self.winLenght = 4
-        # by elimination, this line will only be executed if the chosen size is the regular
-        # one
+
         self.grid = [[] for _ in range(width)]
-        # grid[horizontalCoordonate][verticalCoordonate]
 
     def print(self) -> None:
         '''Print the grid in the CLI'''
@@ -39,7 +29,6 @@ class Board:
         # prints row's numbers
             print(f' {i+1}{" "*(2-len(str(i+1)))}', end='')
         print('')
-        # put the cursor bring the cursor down one line
         for i in (abs(y) for y in range(-self.lenght, 0)):
         # make if so 'i' will iterate form 'self.lenght' to 0
             print('|', end='')
@@ -55,7 +44,6 @@ class Board:
         # check if the collumn is a win
         collumn = self.grid[coord]
         if len(collumn) >= self.winLenght:
-        # there can not be a line of 4 point if the collumn is less than 4 point in lenght
             chain = 0
             # variable to stock the uninterputed line
             for i in range(len(collumn)):
@@ -70,7 +58,6 @@ class Board:
         for i in range(self.width):
             try:
                 if self.grid[i][row] == index: chain += 1
-                # chain++ if the point 
                 else: chain = 0
                 # only pass the 'try' if the point is a player's point
             except IndexError: chain = 0; continue
@@ -83,7 +70,6 @@ class Board:
         c = originalCoord
         # create a copy of 'originalCoord', to be used in the next 10 lines of code
         chain = 0
-        # initiate the counter for the lenght of uninterupted line of the index's pieces
         l = min(self.width-c[0],c[1]+1)
         # the distance the cursor will have to travel to get to the base of the grid
         c = [c[0]+l-1, c[1]-l+1]
@@ -94,12 +80,8 @@ class Board:
         for i in range(l):
             try:
                 if self.grid[c[0]-i][c[1]+i] == index: chain += 1
-                # checks if the point at 'grid[c[1]-i][c[0]+i]' is one of the index's
-                # piece, and add 1 to 'chain' if so
                 else: chain = 0
                 if chain == self.winLenght: return True
-                # stop the fonction and return True if there is an uniterupted line
-                # of four of the index's piece
             except IndexError: chain = 0
             # if the point doesn't exist, catch the error, and make 'chain' = 0
 
@@ -146,7 +128,6 @@ class Player:
         while True:
             try:
                 fullCollumns = list(filter(lambda x: len(grid.grid[x-1])==grid.lenght, [i for i in range(1,grid.width+1)]))
-                # create a list of all the full collumn by comparing their lenght with the grid lenght 
                 if fullCollumns == [i for i in range(1, grid.width+1)]: self.tie = True; return
                 # if all the collumn are full, 'self.tie' = True, and break out of the while
                 coord = int(input(f"Player {self.index+1} : "))
@@ -156,11 +137,7 @@ class Player:
                 if coord in fullCollumns: print('Full collumn, please input another collumn'); continue
                 # if 'coord' doesn't have the right value, make the player input his play one more time
 
-            except ValueError:
-                print('Input unundestandable, please try again')
-                # if 'coord' isn't a integer, make the index input
-                # his play one more time
-                continue
+            except ValueError: print('Input unundestandable, please try again'); continue
             break
         grid.grid[coord-1].append(self.index)
         # add the index of the player to the grid
@@ -168,7 +145,6 @@ class Player:
         # check if the player won, and put the bool in this variable
 
 while True: #SessionLoop
-# allow to continuesly play until player want to stop
     grid = Board()
     # innitiate the board
     playerList = [Player(0),Player(1)]
@@ -177,22 +153,16 @@ while True: #SessionLoop
     # varaible to store the position in 'playerlist' of the player playing
     # It will switch between 0 and 1 to alternate the player
     while True: #GameLoop
-    # while the game isn't stoped by a win or a tie
         grid.print()
         player = playerList[playing]
-        # set the current playing player
         player.play()
-        # get input from player and add it to the grid
         if player.tie:
             grid.print()
-            # print the actuated grid
             print('This game is a tie, well played to both player !')
             break
             # break to get out of the GameLoop and into the BetweenGameLoop
         if player.win:
-        # only execute next 3 lines if the player won
             grid.print()
-            # print the actuated grid
             print(f'Player {playing + 1} won !')
             break
             # break to get out of the GameLoop and into the BetweenGameLoop
@@ -204,9 +174,6 @@ while True: #SessionLoop
         if stop not in 'yn' or len(stop) != 1: print('Input unundestandable, please try again'); continue
         else: break
     if stop == 'n': break
-    # break from SessionLoop <=> quitting the game
     else: continue
-    # only occurs if 'stop' = y, and go back to the start of SessionLoop, restarting
-    # a new game
     
 print('See you next time :)')
